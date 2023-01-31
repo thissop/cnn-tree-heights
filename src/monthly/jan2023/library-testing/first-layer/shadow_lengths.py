9d7577d6395b8c3a78ea9b609819423b3f262d57
Thaddaeus Kiker <68932815+thissop@users.noreply.github.com>
2023-01-30 21:58:37 -0800

    ughhhh...can't get it

--- src/monthly/jan2023/library-testing/first-layer/shadow_lengths.py
+++ src/monthly/jan2023/library-testing/first-layer/shadow_lengths.py
@@ -4,22 +4,68 @@ import geopandas as gpd
 import matplotlib.pyplot as plt 
 from cnnheights.utilities import get_cutline_data
 import matplotlib.pyplot as plt
-from shapely.geometry import LineString
+from shapely.geometry import LineString, LinearRing
+import shapely 
+
+# THIS IS TAKING HOURS TO GET INTERSECTION OF THESE GENERATED LINES WITH THE POLYs LOL
 
 info = get_cutline_data(lat=1706523.96, lon=446623.30)
 annotations_gdf = gpd.read_file('/Users/yaroslav/Documents/Work/NASA/layers/first-working-input/annotations_1.gpkg')
 annotations_gdf.set_crs('epsg:4326', allow_override=True)
 
-
 centroids = annotations_gdf.centroid
 
 d = 3
 dy = np.abs(d/np.tan(np.radians(info['SUN_AZ'])))
-print(dy)
 lines = [LineString([(x-d, x+d), (y+dy, y-dy)]) for x, y in zip(centroids.x, centroids.y)]
-lines_gdf = gpd.GeoDataFrame({'geometry':lines},geometry='geometry', crs=annotations_gdf.crs)
+lines_gdf = gpd.GeoDataFrame({'geometry':lines}, geometry='geometry', crs=annotations_gdf.crs)
 
-print(lines_gdf.iloc[0]['geometry'])
+lines_gdf = lines_gdf.head(1)
+annotations_gdf = annotations_gdf.head(1)
+
+print(gpd.sjoin(lines_gdf, annotations_gdf))
+
+fig, ax = plt.subplots()
+
+annotations_gdf.plot(ax=ax)
+
+print(annotations_gdf.crs, lines_gdf.crs)
+
+new_lines = []
+for i in range(len(lines_gdf.index)):
+    xy = lines_gdf['geometry'][i].xy
+    x = [xy[0][0], xy[1][0]]
+    y = [xy[0][1], xy[1][1]]
+    ax.plot(x, y, color='black', linewidth=0.1)
+    l = LineString([x, y])
+    new_lines.append(l)
+
+    s_poly = annotations_gdf['geometry'][i]
+
+    lring = LinearRing(list(s_poly.exterior.coords))
+
+    #print(l.intersection(annotations_gdf['geometry'].values[0]))
+    #ax.plot(l)
+
+new_lines_gdf = gpd.GeoDataFrame({'geometry':new_lines}, geometry='geometry', crs=annotations_gdf.crs)
+
+new_lines_gdf.plot(ax=ax)
+plt.show()
+
+quit()
+
+for i in range(len(annotations_gdf.index)): 
+    shadow_line = lines_gdf['geometry'][i]
+    print(shadow_line.length)
+    shadow_annotation = annotations_gdf['geometry'][i]
+    #print(intersection(shadow_line, shadow_annotation))
+    
+    inside = lines_gdf['geometry'][i].intersection(annotations_gdf.iloc[i]['geometry']).length
+    
+    
+    #print(lines_gdf.iloc[i]['geometry'])
+    #print(annotations_gdf.iloc[i]['geometry'])
+    #print(inside)
 
 fig, ax = plt.subplots()
 
