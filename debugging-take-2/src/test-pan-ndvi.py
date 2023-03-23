8bce60030c19a68cd50016e315d8efb9852a31e5
thissop <600018686@fjuhsd.org>
2023-03-23 19:03:05 -0400

    confirming pan is messed up need to fix

--- debugging-take-2/src/test-pan-ndvi.py
+++ debugging-take-2/src/test-pan-ndvi.py
@@ -2,19 +2,21 @@ import geopandas as gpd
 import rasterio 
 import matplotlib.pyplot as plt 
 
-annotation = '/ar1/PROJ/fjuhsd/personal/thaddaeus/github/cnn-tree-heights/cnn-input/extracted_annotation_0.png'
-boundary = annotation.replace('annotation', 'boundary')
-ndvi = annotation.replace('annotation', 'ndvi')
-pan = annotation.replace('annotation', 'pan')
+fig, axs = plt.subplots(4,4, figsize=(7,7))
 
-fig, axs = plt.subplots(4,1, figsize=(2,7))
+for i in range(4): 
 
-files = [annotation, boundary, ndvi, pan]
-titles = ['annotation', 'boundary', 'ndvi', 'pan']
-for i in range(len(files)):
-    img = rasterio.open(files[i]).read(1)
-    axs[i].imshow(img, cmap='magma')
-    axs[i].set(title=titles[i])
+    annotation = f'/ar1/PROJ/fjuhsd/personal/thaddaeus/github/cnn-tree-heights/data/input/extracted_annotation_{i}.png'
+    boundary = annotation.replace('annotation', 'boundary')
+    ndvi = annotation.replace('annotation', 'ndvi')
+    pan = annotation.replace('annotation', 'pan')
+
+    files = [annotation, boundary, ndvi, pan]
+    titles = ['annotation', 'boundary', 'ndvi', 'pan']
+    for j in range(len(files)):
+        img = rasterio.open(files[j]).read(1)
+        axs[i, j].imshow(img, cmap='magma')
+        axs[i, j].set(title=titles[j])
 
 plt.tight_layout()
 plt.savefig('/ar1/PROJ/fjuhsd/personal/thaddaeus/github/cnn-tree-heights/debugging-take-2/output/plots/data-check.png', dpi=250)
