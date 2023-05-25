7a99145ec079eba92c0947b44bbc86eba8ba7df4
jrmeyer3 <jesse.r.meyer@nasa.org>
2023-05-25 09:31:19 -0400

    Finish initial stage 4 testing, and introduce stage 5.
    Stage 4 is missing the critical component of actually determining the shadow length and associated tree height, but the context has now been established for further development.
    
    Stage 5 now exists, ta da, but in TODO form.  Determining tree heights alone is publishable, but I suspect a top tier journal publication if we can combine the heights with area to better quantify total carbon in the study region.

--- cnnheights/stage_4_heights_determination.py
+++ cnnheights/stage_4_heights_determination.py
@@ -1,7 +1,9 @@
 #NOTE(Jesse):  The premise is to transform the CNN detection mask into polygons from which shadow lengths and heights
 # are estimated from the associated mosaic cutline geometry's viewing and solar geometry.
 
-UNTESTED
+tile_shp_fp = "/path/to/mosaic_tile.shp"
+nn_result_fp = "/path/to/nn_results.tif"
+out_dir = "/path/to/output/"
 
 tile_shp_fp = "/path/to/mosaic/cutline.shp"
 nn_result_fp = "/path/to/nn_result.tif"
@@ -36,7 +38,7 @@ def main():
     gdal.SetCacheMax(0)
 
     nn_dsk_ds = gdal.Open(nn_result_fp)
-    nn_mem_ds = gdal.GetDriverByName("MEM").CreateCopy(nn_dsk_ds)
+    nn_mem_ds = gdal.GetDriverByName("MEM").CreateCopy("", nn_dsk_ds)
     nn_mem_band = nn_mem_ds.GetRasterBand(1)
 
     arr = nn_mem_band.ReadAsArray()
@@ -48,12 +50,12 @@ def main():
 
     nn_dsk_ds = None
 
-    nn_mem_geo_ds = ogr.GetDriverByName("Memory").CreateDataSource("", None)
-    nn_mem_geo_lyr = nn_mem_geo_ds.CreateLayer("shadows", gdal.GetSpatialRef(nn_mem_ds), ogr.wkbPolygon, None)
+    nn_mem_geo_ds = ogr.GetDriverByName("Memory").CreateDataSource("")
+    nn_mem_geo_lyr = nn_mem_geo_ds.CreateLayer("shadows", nn_mem_ds.GetSpatialRef(), ogr.wkbPolygon)
 
     new_field_names = ("shadow length", "height")
     shadow_length_idx = 0
-    height_idx = 0
+    height_idx = 1
     for fn in new_field_names:
         nn_mem_geo_lyr.CreateField(ogr.FieldDefn(fn, ogr.OFTReal))
 
@@ -62,20 +64,44 @@ def main():
     nn_mem_band = None
     nn_mem_ds = None
 
-    cutline_mem_ds = ogr.GetDriverByName("Memory").Open(tile_shp_fp)
-    cutline_lyr = cutline_mem_ds.GetLayer(1)
+    cutline_dsk_ds = ogr.Open(tile_shp_fp)
+    cutline_mem_ds = ogr.GetDriverByName("Memory").CopyDataSource(cutline_dsk_ds, "") #NOTE(Jesse): Yes, OGR and GDAL swap param order here.
+    cutline_dsk_ds = None
+    cutline_lyr = cutline_mem_ds.GetLayer(0)
 
-    #NOTE(Jesse): debug
-    c_lyr_defn = cutline_lyr.GetLayerDefn()
-    for i in range(c_lyr_defn.GetFieldCount()):
-        print(f"{c_lyr_defn.GetFieldDefn(i).GetName()}: {i}")
+    debug = False
+    if debug:
+        c_lyr_defn = cutline_lyr.GetLayerDefn()
+        for i in range(c_lyr_defn.GetFieldCount()):
+            print(f"{c_lyr_defn.GetFieldDefn(i).GetName()}: {i}")
 
-    off_nadir_idx = 0
-    sun_elevation_idx = 1
-    #DO ASSERTS
+    #NOTE(Jesse): Field indices, via the above debug code
+    cat = 0
+    IMAGENAME = 1
+    SENSOR = 2
+    ACQDATE = 3
+    CAT_ID = 4
+    RESOLUTION = 5
+    OFF_NADIR = 6
+    SUN_ELEV = 7
+    SUN_AZ = 8
+    SAT_ELEV = 9
+    SAT_AZ = 10
+    CLOUDCOVER = 11
+    TDI = 12
+    DATE_DIFF = 13
+    SCORE = 14
+    SCAN_DIR = 15
+    RF_MULT = 16
+    RF_ADD = 17
+    STATS_MIN = 18
+    STATS_MAX = 19
+    STATS_STD = 20
+    STATS_MEAN = 21
+    STATS_PXCT = 22
 
     set_feature = nn_mem_geo_lyr.SetFeature
-    for c_ftr in cutline_lyr:
+    for i, c_ftr in enumerate(cutline_lyr):
         c_geo = c_ftr.GetGeometryRef()
         if not c_geo.IsValid():
             #TODO(Jesse): Investigate how to robustly recover the geometry via MakeValid
@@ -84,19 +110,25 @@ def main():
         if c_geo.Area() < 300:
             continue #NOTE(Jesse): Arbiturary threshold to skip small cutline geometries
 
-        off_nadir = c_ftr.GetFieldAsDouble(off_nadir_idx)
-        sun_elevation = c_ftr.GetFieldAsDouble(sun_elevation_idx)
+        off_nadir = c_ftr.GetFieldAsDouble(OFF_NADIR)
+        sun_elevation = c_ftr.GetFieldAsDouble(SUN_ELEV)
 
         nn_mem_geo_lyr.SetSpatialFilter(c_geo)
         for n_ftr in nn_mem_geo_lyr:
             #TODO(Jesse): Shadow length and height estimation
-            shadow_length = 1
-            height = 1
+            shadow_length = i
+            height = i
             n_ftr.SetField(shadow_length_idx, shadow_length)
             n_ftr.SetField(height_idx, height)
             set_feature(n_ftr)
 
-    ds = ogr.GetDriverByName("GPKG").CreateCopy(join(out_dir, "heights.gpkg"), nn_mem_geo_ds)
+    nn_mem_geo_lyr.SetSpatialFilter(None)
+    nn_mem_geo_lyr = None
+    ds = ogr.GetDriverByName("GPKG").CopyDataSource(nn_mem_geo_ds, join(out_dir, "heights.gpkg"))
     ds = None
 
+from time import time
+start = time()
 main()
+stop = time()
+print(f"Took {(stop - start) / 60} minutes")
