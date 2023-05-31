96b9eaefd29a0f810b509d7ec84853424592e7cd
Jesse Meyer <jesse.r.meyer@nasa.gov>
2023-05-31 13:09:02 -0400

    UNTESTED Merge Stage 4 with Stage 3
    On reflection, stage 3 should output vector geometries alongside the raster mask predictions -- the data is already there in memory.  This saves a step in the pipeline.

--- cnnheights/stage_3_predict.py
+++ cnnheights/stage_3_predict.py
@@ -1,9 +1,9 @@
 #NOTE(Jesse): The premise is to apply a trained CNN to detect tree canopies in the provided mosaic tile.
+# The outputs are a raster and vector dataset of the predicted shadow geometries.
 
 tile_fp = "/path/to/tile.tif"
 out_fp = "/path/to/destination/"
 model_weights_fp = "/path/to/weights.h5"
-
 
 if __name__ != "__main__":
     print(f"This script {__name__} must be called directly and not imported to be used as a library.  Early exiting.")
@@ -12,9 +12,10 @@ if __name__ != "__main__":
 def main():
     from os.path import isfile, isdir, normpath, join
     from numpy import zeros, float32, ceil, uint8, maximum, nan, nanmean, nanstd, squeeze, isnan
-    from osgeo import gdal
+    from osgeo import gdal, ogr
 
     gdal.UseExceptions()
+    ogr.UseExceptions()
 
     gdal.SetConfigOption("GDAL_DISABLE_READDIR_ON_OPEN", "TRUE")
     gdal.SetConfigOption("GDAL_NUM_THREADS", "ALL_CPUS")
@@ -78,34 +79,40 @@ def main():
     for y0 in range(0, tile_y, step):
         y1 = min(y0 + 256, tile_y)
 
-        pan_batch = pan[y0:y1, :]
-        ndvi_batch = ndvi[y0:y1, :]
+        pan_strip = pan[y0:y1, :]
+        ndvi_strip = ndvi[y0:y1, :]
         for batch_idx, x0 in enumerate(range(0, tile_x, step)):
             x1 = min(x0 + 256, tile_x)
 
-            batch[batch_idx, ..., 0] =  standardize(pan_batch[:, x0:x1])
-            batch[batch_idx, ..., 1] = standardize(ndvi_batch[:, x0:x1])
+            batch[batch_idx, ..., 0] =  standardize(pan_strip[:, x0:x1])
+            batch[batch_idx, ..., 1] = standardize(ndvi_strip[:, x0:x1])
 
         predictions = squeeze(model.predict(batch, batch_size=16)) * 100
         batch.fill(0)
 
-        out_predictions_batch = out_predictions[y0:y1, :]
+        out_predictions_strip = out_predictions[y0:y1, :]
         for i, x0 in enumerate(range(0, tile_x, step)):
             x1 = min(x0 + 256, tile_x)
 
-            op = out_predictions_batch[:, x0:x1]
+            op = out_predictions_strip[:, x0:x1]
             p = predictions[i].astype(uint8)
 
             if op.shape != p.shape: #NOTE(Jesse): Handle fractional step patches
                 p.resize(op.shape, refcheck=False)
 
-            out_predictions_batch[:, x0:x1] = maximum(op, p)
+            out_predictions_strip[:, x0:x1] = maximum(op, p)
 
     no_data_value = 255
-    out_predictions[(ndvi == 0) & (out_predictions <= 50)] = no_data_value #NOTE(Jesse): Transfer no-data value fromo mosaic tile to these results.
+    out_predictions[(ndvi == 0) & (out_predictions <= 50)] = no_data_value #NOTE(Jesse): Transfer no-data value from mosaic tile to these results.
 
+    model = None
+
+    predictions = None
+    pan_strip = None
     pan = None
+    ndvi_strip = None
     ndvi = None
+    out_predictions_strip = None
     batch = None
 
     nn_mem_ds = gdal.GetDriverByName("MEM").Create('', xsize=tile_x, ysize=tile_y, bands=1, eType=gdal.GDT_Byte)
@@ -114,14 +121,47 @@ def main():
     nn_mem_ds.GetRasterBand(1).SetNoDataValue(no_data_value)
     nn_mem_ds.SetGeoTransform(geotransform)
     nn_mem_ds.SetProjection(out_projection)
-    nn_mem_ds.GetRasterBand(1).WriteArray(out_predictions)
+    nn_mem_band = nn_mem_ds.GetRasterBand(1)
 
-    out_predictions = None
+    #NOTE(Jesse): Convert raster predictions to vector geometries via binary mask of >50% thresholding
+    arr = out_predictions.copy()
+
+    arr[arr == no_data_value] = 0
+    arr[arr <= 50] = 0
+    arr[arr > 50] = 1
+    nn_mem_band.WriteArray(arr)
+    arr = None
+
+    shadow_mem_ds = ogr.GetDriverByName("Memory").CreateDataSource("")
+    shadow_mem_lyr = shadow_mem_ds.CreateLayer("shadows", nn_mem_ds.GetSpatialRef(), ogr.wkbPolygon)
+    shadow_mem_lyr.CreateField(ogr.FieldDefn("shadow length", ogr.OFTReal))
+
+    gdal.Polygonize(nn_mem_band, nn_mem_band, shadow_mem_lyr, -1)
+
+    #shadow_mem_lyr.StartTransaction()
+    set_feature = shadow_mem_lyr.SetFeature
+    for i, ftr in enumerate(shadow_mem_lyr):
+        geo = ftr.GetGeometryRef()
+        s_geo = geo.SimplifyPreserverTopology(1) #NOTE(Jesse): Merge vertices <= 1 meter distant (~two pixels)
+        assert s_geo.IsValid(), i
+
+        ftr.SetGeometry(s_geo)
+
+        set_feature(ftr)
+        s_geo = None
+        geo = None
+    ftr = None
+    #shadow_mem_lyr.CommitTransaction()
+
+    shadows_ds = ogr.GetDriverByName("GPKG").CopyDataSource(shadow_mem_ds, join(out_fp, "shadows.gpkg"))
+    shadow_mem_lyr = None
+    shadow_mem_ds = None
+    shadows_ds = None
 
     disk_create_options: list = ['COMPRESS=ZSTD', 'ZSTD_LEVEL=1', 'INTERLEAVE=BAND', 'Tiled=YES', 'NUM_THREADS=ALL_CPUS', 'SPARSE_OK=True', 'PREDICTOR=2']
+    nn_mem_band.WriteArray(out_predictions)
+    out_predictions = None
     nn_disk_ds = gdal.GetDriverByName('GTiff').CreateCopy(join(out_fp, 'NN_classification.tif'), nn_mem_ds, options=disk_create_options)
-    assert nn_disk_ds
-
     nn_mem_ds = None
     nn_disk_ds = None
 
